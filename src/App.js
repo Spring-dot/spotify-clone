fc76e568d868d0c23fdbf103b79abe4227ecc822
Akanksha priya <58500059+Spring-dot@users.noreply.github.com>
2022-01-01 17:02:48 +0530

    added auth page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React, { useEffect } from 'react';
 import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  //useeffect is run the code under given condition
+  useEffect(() => {
+    const token = getTokenFromUrl();
+    
+    console.log("this is token", token);
+    
+  }, []);
+
+
   return (
     <div className="App">
-      <h1> . </h1>
+      <Login />
+       
     </div>
   );
 }
