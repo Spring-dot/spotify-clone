0aef1b72bf85eba6f92d026a757346290c87f1c1
Akanksha priya <58500059+Spring-dot@users.noreply.github.com>
2022-01-02 23:29:13 +0530

    worked on accesstoken

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from './Player';
+
+// Spotify is our major object to use spotifyWebApi throughout the project
+const spotify = new SpotifyWebApi(); 
 
 function App() {
+  const [token, setToken] = useState(null);
+
+
   //useeffect is run the code under given condition
   useEffect(() => {
-    const token = getTokenFromUrl();
-    
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+
+      //this is the secret key to move and control back and forth between react-project and spotify-API
+      spotify.setAccessToken(_token);
+
+      //to access user from spotify-webAPI
+      spotify.getMe().then(user => {
+        console.log('user:', user);
+      });
+    }
+
     console.log("this is token", token);
     
   }, []);
@@ -15,7 +37,15 @@ function App() {
 
   return (
     <div className="App">
-      <Login />
+      {
+        token ? (
+          <Player />
+          //<h1>this to show i m logged in</h1>
+        ) : (
+          <Login />
+        )
+      }
+     
        
     </div>
   );
---
+++ src/Player.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Player() {
+    return (
+        <div>
+            this is player page
+        </div>
+    )
+}
+
+export default Player
